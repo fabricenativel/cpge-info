80a0144c7e3168f9e2c787a94657d52a1ff68b5e
fabricenativel <fnativel2@ac-reunion.fr>
2024-02-02 21:21:12 +0400

    jpo

--- docs/mp2i/files/C12/arbres_binaires.c
+++ docs/mp2i/files/C12/arbres_binaires.c
@@ -1,63 +1,109 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<unistd.h>
 
 struct noeud
 {
-    struct noeud* sag;
-    char value;
-    struct noeud* sad;
+    struct noeud *sag;
+    int valeur;
+    struct noeud *sad;
 };
 typedef struct noeud noeud;
-typedef noeud * arbre_binaire;
+typedef noeud *ab;
 
-arbre_binaire cree_arbre( arbre_binaire g, int v, arbre_binaire d)
+int ninv = 0;
+
+ab cree_arbre(ab g, int v, ab d)
 {
     noeud *n = malloc(sizeof(noeud));
     n->sag = g;
-    n->value = v;
+    n->valeur = v;
     n->sad = d;
     return n;
 }
 
-void write_edge(FILE * writer, arbre_binaire g)
+int taille(ab a)
 {
-    if (g->sag!=NULL)
+    if (a==NULL)
+    { return 0;}
+    return 1 + taille(a->sag) + taille(a->sad);;
+}
+
+void write_edge(FILE *writer, ab g)
+{
+    if (g->sag != NULL)
     {
-        fprintf(writer,"%c",g->value);
-        fprintf(writer," -> ");
-        fprintf(writer,"%c\n",g->sag->value);
-        write_edge(writer,g->sag);
+        fprintf(writer, "%d", g->valeur);
+        fprintf(writer, " -> ");
+        fprintf(writer, "%d\n", g->sag->valeur);
+        write_edge(writer, g->sag);
     }
-    if (g->sag!=NULL)
+    else
     {
-        fprintf(writer,"%c",g->value);
-        fprintf(writer," -> ");
-        fprintf(writer,"%c\n",g->sad->value);
-        write_edge(writer,g->sad);
+        fprintf(writer, "I%d [style=invis]\n", ninv);
+        fprintf(writer, "%d -> I%d [style=invis]\n", g->valeur, ninv);
+        ninv++;
+    }
+    fprintf(writer, "I%d [style=invis]\n", ninv);
+    fprintf(writer, "%d -> I%d [style=invis]\n", g->valeur, ninv);
+    ninv++;
+    if (g->sad != NULL)
+    {
+        fprintf(writer, "%d", g->valeur);
+        fprintf(writer, " -> ");
+        fprintf(writer, "%d\n", g->sad->valeur);
+        write_edge(writer, g->sad);
+    }
+    else
+    {
+        fprintf(writer, "I%d [style=invis]\n", ninv);
+        fprintf(writer, "%d -> I%d [style=invis]\n", g->valeur, ninv);
+        ninv++;
     }
 }
 
-void make_dot(arbre_binaire g)
+void view(ab g)
 {
-    FILE * writer= fopen("temp.gv","w");
+    char buf[256];
+    int n;
+    FILE *writer = fopen("temp.gv", "w");
+    FILE *imgwriter = fopen("temp.jpg", "wb");
     fprintf(writer, "digraph mygraph {\n");
-    write_edge(writer,g);
-    fprintf(writer,"}\n");
-    printf("Return value de dot =%d\n",system("dot temp.gv -Tjpg | display"));
-    sleep(1);
-    system("eog \"temp.png\"");
-    //system("rm \"test.png\"");
-    //system("rm \"test.gv\"");
+    write_edge(writer, g);
+    fprintf(writer, "}\n");
+    fclose(writer);
+    FILE *dotresult = popen("dot temp.gv -Tjpg", "r");
+    if (dotresult == NULL)
+    {
+        printf("Bug !\n");
+    }
+    while ((n = fread(buf, 1, 256, dotresult)) != 0)
+    {
+        fwrite(buf, 1, n, imgwriter);
+    }
+    pclose(dotresult);
+    fclose(imgwriter);
+    system("eog temp.jpg &");
 }
 
-
-
+void prefixe(ab a)
+{
+    if (a!=NULL)
+    {
+        printf("%d ",a->valeur);
+        prefixe(a->sag);
+        prefixe(a->sad);
+    }
+}
 
 int main()
 {
-    arbre_binaire c = cree_arbre(NULL,'c',NULL);
-    arbre_binaire b = cree_arbre(NULL,'b',NULL);
-    arbre_binaire a = cree_arbre(b, 'a', c);
-    make_dot(a);
+    ab n7 = cree_arbre(NULL, 7, NULL);
+    ab n5 = cree_arbre(NULL, 5, n7);
+    ab n4 = cree_arbre(NULL, 4, NULL);
+    ab n3 = cree_arbre(n4, 3, NULL);
+    ab n9 = cree_arbre(n5, 9, n3);
+    view(n9);
+    printf("Taille de l'arbre = %d\n",taille(n9));
+    prefixe(n9);
+    printf("\n");
 }
---
+++ docs/mp2i/files/C12/arbres_binaires.ml
@@ -0,0 +1,78 @@
+type ab = 
+ |Vide 
+ |Noeud of ab *  char * ab;;
+let ninv = ref 0;;
+
+let rec taille ab =
+  match ab with
+  | Vide -> 0
+  | Noeud (sag, _, sad) -> 1 + taille sag + taille sad;;
+
+let rec write_edge a writer =
+    match a with
+    | Vide -> ()
+    | Noeud (sag,e,sad) ->
+      match sag, sad with
+      | Vide, Vide -> ();
+      | Noeud (_, eg, _), Vide ->
+          Printf.fprintf writer "%c -> %c\n" e eg;
+          Printf.fprintf writer  "I%d [style=invis]\n %c -> I%d [style=invis]\n" !ninv e !ninv;
+          ninv := !ninv +1;
+          Printf.fprintf writer  "I%d [style=invis]\n %c -> I%d [style=invis]\n" !ninv e !ninv;
+          ninv := !ninv +1;
+          write_edge sag writer;
+      | Vide, Noeud(_,ed,_) ->
+          Printf.fprintf writer  "I%d [style=invis]\n %c -> I%d [style=invis]\n" !ninv e !ninv;
+        ninv := !ninv +1;
+          Printf.fprintf writer  "I%d [style=invis]\n %c -> I%d [style=invis]\n" !ninv e !ninv;
+          ninv := !ninv +1;
+          Printf.fprintf writer "%c -> %c\n" e ed;
+          write_edge sad writer;
+      | Noeud (_,eg,_), Noeud (_,ed, _ ) ->
+          Printf.fprintf writer "%c -> %c\n" e eg;
+          Printf.fprintf writer  "I%d [style=invis]\n %c -> I%d [style=invis]\n" !ninv e !ninv;
+          ninv := !ninv +1;
+          Printf.fprintf writer "%c -> %c\n" e ed;
+          write_edge sag writer;
+          write_edge sad writer;
+      ;;
+
+let view a =
+  let outc = open_out "temp.gv" in
+  output_string outc "digraph mygraph {\n graph [nodesep=3, ranksep=3]";
+  write_edge a outc;
+  output_string outc "}\n";
+  close_out outc;
+  Sys.command "dot -Tpng temp.gv -o temp.png";
+  Sys.command "eog temp.png";;
+  
+
+let rec prefixe a =
+  match a with
+  | Vide -> ()
+  | Noeud (g,e,d) -> Printf.printf "%c " e; prefixe g; prefixe d;;
+
+
+let rec infixe a =
+  match a with
+  | Vide -> ()
+  | Noeud (g,e,d) ->  infixe g; Printf.printf "%c " e; infixe d;;
+
+  let rec postfixe a =
+    match a with
+    | Vide -> ()
+    | Noeud (g,e,d) ->  postfixe g; postfixe d; Printf.printf "%c " e;;
+
+let () =
+let  a = Noeud (
+  Noeud(Noeud(Vide,'V',Noeud(Vide,'M',Vide)),'T',Noeud(Vide,'I',Vide)),
+  'R',
+  Noeud(Noeud(Noeud(Vide,'P',Vide),'F',Noeud(Vide,'A',Vide)),'C',Vide)
+  ) in
+prefixe a;
+print_newline();
+infixe a;
+print_newline();
+postfixe a;
+print_newline();
+Printf.printf "Taille : %d\n" (taille a);
