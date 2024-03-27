52bd8fe02b65bd65cf7fae5788d30110c9106558
fabricenativel <fnativel2@ac-reunion.fr>
2024-03-27 20:19:19 +0400

    C15

--- docs/mp2i/files/C15/barre.c
+++ docs/mp2i/files/C15/barre.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <time.h>
 
 void lire_barre(char *fname, int barre[], int size)
 {
@@ -18,11 +19,11 @@ void lire_barre(char *fname, int barre[], int size)
 // Découpe maximale d'une barre de taille n
 int vmax(int barre[], int n) {
     int cmax = 0;
-    int vk;
+    int vnk;
     if (n == 0) {return 0;}
-    for (int k = 0; k < n; k++){
-        vk = vmax(barre, k);
-        if (barre[n-k] + vk > cmax) {cmax = vk + barre[n-k];}
+    for (int k = 1; k <= n; k++){
+        vnk = vmax(barre, n-k);
+        if (barre[k] + vnk > cmax) {cmax = vnk + barre[k];}
         }
     return cmax;}
 
@@ -30,26 +31,51 @@ int vmax(int barre[], int n) {
 int vmax_memo(int barre[], int n, int v[]){
     if (v[n]!=-1) {return v[n];}
     int cmax = 0;
-    int vk;
-    for (int k = 0; k < n; k++){
-        vk = vmax_memo(barre, k, v);
-        if (barre[n-k] + vk > cmax) {cmax = vk + barre[n-k];}
+    int vnk;
+    for (int k = 1; k <= n; k++){
+        vnk = vmax_memo(barre, n-k, v);
+        if (barre[k] + vnk > cmax) {cmax = vnk + barre[k];}
         }
     v[n] = cmax;
     return cmax;}
 
+
+int vmax_iter(int barre[], int n){
+    int vmax[n];
+    vmax[0] = 0;
+    vmax[1] = barre[1];
+    for (int i=2; i<=n; i++){
+        vmax[i] = 0;
+        for (int j=1; j<=i; j++){
+            if (vmax[j]+barre[i-j]>vmax[i])
+            {vmax[i] = vmax[j]+barre[i-j];}}}
+    return vmax[n];}
+    
+
 int main()
 {
-    int bsize = 13;
-    int barre[bsize];
-    int v[bsize];
-    for (int k=0;k<bsize;k++)
+    int bsize = 12;
+    int barre[bsize+1];
+    int v[bsize+1];
+    float st,et;
+    int res;
+    for (int k=0;k<bsize+1;k++)
     {
         v[k] = -1;
     }
     v[0] = 0;
-    lire_barre("ex1.txt", barre, bsize);
-    printf("Solution = %d \n", vmax(barre, 12));
-    printf("Solution (mémo) = %d \n", vmax_memo(barre, 12, v));
+    lire_barre("ex1.txt", barre, bsize+1);
+    st = (float)clock() / CLOCKS_PER_SEC;
+    res = vmax(barre, bsize);
+    et = (float)clock() / CLOCKS_PER_SEC;
+    printf("Solution = %d obtenue en %f s\n", res, et-st);
+    st = (float)clock() / CLOCKS_PER_SEC;
+    res = vmax_memo(barre, bsize, v);
+    et = (float)clock() / CLOCKS_PER_SEC;
+    printf("Solution (mémo) = %d obtenue en %f s\n", res, et-st);
+    st = (float)clock() / CLOCKS_PER_SEC;
+    res = vmax_iter(barre, bsize);
+    et = (float)clock() / CLOCKS_PER_SEC;
+    printf("Solution (itérative) = %d obtenue en %f s\n", res, et-st);
 
 }
