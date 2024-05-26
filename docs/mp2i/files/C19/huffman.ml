099a9bfd25d7312988b7f488539075985a152c46
fenarius <nativelfabrice@gmail.com>
2024-05-26 19:35:19 +0400

    hu

---
+++ docs/mp2i/files/C19/huffman.ml
@@ -0,0 +1,99 @@
+type ab = Feuille of int | Noeud of ab*ab;;
+
+type heap_int = {mutable size : int; data : int array};;
+
+type pqueue = 
+{
+  mutable size : int;
+  data : (int*ab) array;
+};;
+
+let leftchild i n = let s = 2*i+1 in if s<n then s else -1
+
+let parent i = let p = (i-1)/2 in if i<>0 then p else -1
+
+let swap tab i j =
+  let temp = tab.(i) in
+  tab.(i) <- tab.(j);
+  tab.(j) <- temp;;
+
+  let insert v heap =
+    let cp = ref heap.size in
+    heap.data.(!cp) <- v;
+    while (parent !cp) <> (-1)  && (fst heap.data.(parent !cp) > fst heap.data.(!cp)) do
+        swap heap.data !cp (parent !cp);
+        cp := parent !cp;
+    done;
+    heap.size <- heap.size + 1;;
+
+let nbchar = 127;;
+
+let frequence texte =
+  let fq = Array.make nbchar 0 in
+  for i=0 to String.length texte - 1 do
+    fq.(int_of_char texte.[i]) <- fq.(int_of_char texte.[i]) + 1;
+  done;
+  fq;;
+
+  let extract_min theap =
+    let minv = theap.data.(0) in
+    theap.data.(0) <- theap.data.(theap.size-1);
+    let cp = ref 0 in
+    let lc = ref (leftchild !cp theap.size) in
+    while ( !lc <> (-1) && (fst theap.data.(!lc) < fst theap.data.(!cp) || (!lc+1 < theap.size && fst theap.data.(!lc+1) < fst theap.data.(!cp)))) do
+      if (!lc+1 >= theap.size || fst theap.data.(!lc)< fst theap.data.(!lc+1)) then
+       (swap theap.data !cp !lc;
+        cp := !lc)
+      else  
+        (swap theap.data !cp (!lc+1);
+        cp := !lc+1);
+      lc := leftchild !cp theap.size;
+    done;
+    theap.size <- theap.size -1;
+    minv;;
+
+let construire_arbre fq =
+  let todo = {size = 0; data = Array.make nbchar (0,Feuille(0))} in
+  for i=0 to nbchar-1 do
+    if fq.(i)<>0 then insert (fq.(i),Feuille(i)) todo;
+  done;
+  let  br1 = ref (0,Feuille(0)) in
+  let  br2 = ref (0,Feuille(0)) in
+  let code_abr = ref  (0,Feuille(0)) in
+  while (todo.size <> 1) do
+    br1 := extract_min todo;
+    br2 := extract_min todo;
+    code_abr := (fst !br1 + fst !br2, Noeud(snd !br1, snd !br2));
+  insert !code_abr todo;
+  done;
+  !code_abr;;
+
+let cree_code code_abr =
+  let code = Array.make nbchar "" in
+  let rec aux_cc code_abr ch =
+    match code_abr with
+  |   _,Feuille(n) -> (code.(n) <- ch;)
+  |   _,Noeud(g,d) -> (aux_cc (0,g) ("0"^ch); aux_cc (0,d) ("1"^ch) ;)
+  in
+  aux_cc code_abr "";
+  code;;
+;;
+
+let () = let test ="satisfaisant" in
+            let fq = frequence test in
+            Printf.printf "Calcul des fréquences ok !\n";
+            let ab_code = construire_arbre fq in
+            Printf.printf "Construction arbre ok ! \n";
+            let code = cree_code (ab_code) in
+            for i=0 to nbchar-1 do
+              if code.(i)<>"" then Printf.printf "Caractère : %c : %s\n" (char_of_int i) code.(i);
+            done;
+
+
+    
+
+  
+
+  
+
+
