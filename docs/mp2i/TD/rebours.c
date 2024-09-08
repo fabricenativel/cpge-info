eabd865be04bbc4aa7aa9122c8d35f7cab945dd0
fabricenativel <fnativel2@ac-reunion.fr>
2024-09-08 10:48:38 +0400

    c2

---
+++ docs/mp2i/TD/rebours.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdint.h>
+
+// Compte à rebours de n à 0 et afficher "Partez !"
+void compte_rebours(int n)
+{
+    while (n != 0)
+    {
+        printf("%d \n",n);
+        n = n-1;
+    }
+    printf("Partez !\n");
+}
+
+int main()
+{
+    compte_rebours(10);
+}
