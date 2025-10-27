410d182983dee3ddf6be239e2320648015fa1b46
fabricenativel <fnativel2@ac-reunion.fr>
2025-10-27 12:54:02 +0400

    harshad

--- docs/mp2i/Evaluations/TP/TP2/harshad.c
+++ docs/mp2i/Evaluations/TP/TP2/harshad.c
@@ -9,25 +9,25 @@ bool est_harshad(int n)
     int num = n;
     while (num != 0)
     {
-        sum += (num%10);
+        sum += (num % 10);
         num = num / 10;
     }
     return (n % sum == 0);
 }
 
-int * get_harshad(int limit, int *size)
+int *get_harshad(int limit, int *size)
 {
     *size = 0;
-    for (int i=1;i<=limit;i++)
+    for (int i = 1; i <= limit; i++)
     {
         if (est_harshad(i))
         {
             *size = *size + 1;
         }
     }
-    int *harshad = malloc(sizeof(int)*(*size));
+    int *harshad = malloc(sizeof(int) * (*size));
     int cpt = 0;
-    for (int i=1;i<limit;i++)
+    for (int i = 1; i < limit; i++)
     {
         if (est_harshad(i))
         {
@@ -42,16 +42,16 @@ int consecutifs(int tab[], int size, int *start)
 {
     int cpt = 1;
     int maxc = 1;
-    int  maxs=1;
-    for (int i=11;i<size;i++)
+    int maxs = 1;
+    for (int i = 11; i < size; i++)
     {
-        if (tab[i]==tab[i-1] + 1)
+        if (tab[i] == tab[i - 1] + 1)
         {
             cpt += 1;
-            if (cpt>maxc)
+            if (cpt > maxc)
             {
                 maxc = cpt;
-                maxs = tab[i-cpt+1];
+                maxs = tab[i - cpt + 1];
             }
         }
         else
@@ -66,25 +66,28 @@ int consecutifs(int tab[], int size, int *start)
 void affiche(int tab[], int size)
 {
     printf("{");
-    for (int i = 0; i< size-1;i++)
+    for (int i = 0; i < size - 1; i++)
     {
-        printf("%d, ",tab[i]);
+        printf("%d, ", tab[i]);
     }
-    printf("%d}\n",tab[size-1]);
+    printf("%d}\n", tab[size - 1]);
 }
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    if (argc!=2)
+    if (argc != 2)
     {
-        printf("Utilisation : %s <entier> \n",argv[0]);
-        exit(-1);
+        printf("Utilisation : %s <entier> \n", argv[0]);
+        return EXIT_FAILURE;
     }
-    int n= atoi(argv[1]);
-    int m,s;
-    int *h = get_harshad(n,&m);
-    printf("%d nombres de harshad <= %d\n",m, n);
-    int c = consecutifs(h,m,&s);
-    printf("%d nombres de harshad consécutifs trouvés depuis %d\n",c,s);
-    free(h);
+    int n = atoi(argv[1]);
+    if (est_harshad(n))
+    {
+        printf("true\n");
+    }
+    else
+    {
+        printf("false\n");
+    }
+    return EXIT_SUCCESS;
 }
