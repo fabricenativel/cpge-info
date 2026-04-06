3b03b552c9d2bf07fffaa498deb98486d37fe01a
fabricenativel <fnativel2@ac-reunion.fr>
2026-04-06 17:14:56 +0400

    DS5

--- docs/mp2i/Evaluations/DS/DS5/ack.ml
+++
@@ -1,9 +0,0 @@
-let rec ack n m =
-  match n, m with
-  | 0, m -> m + 1
-  | n, 0 -> ack (n - 1) 1
-  | n, m -> ack (n - 1) (ack n (m - 1));;
-
-type 'a arbrebin =
-  | Vide
-  | Noeud of  'a arbrebin * 'a * 'a arbrebin
